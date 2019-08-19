44a585f2f57244dd52b211071d0efba94290b155
Raghavendra Guttur <strikingraghu@gmail.com>
2019-08-19 23:03:17 +0530

    Python

--- Arrays.py
+++ Arrays.py
@@ -7,7 +7,13 @@ They can be useful when we have to manipulate only a specific data type values.
 import array
 
 # initialize
-one_array = array.array('i', [1, 2, 3, 4, 5, 55, 33, 9, 3, 3, 55, 81])     # Order Maintained/Duplicates Allowed
+one_array = array.array('i', [1, 2, 3, 4, 5, 55, 33, 9, 3, 3, 55, 81])  # Order Maintained/Duplicates Allowed
 print("Printing Array:", one_array)
 print("Array Size:", one_array.__sizeof__())
+print("Length:", one_array.__len__())
+one_array.append(100)  # Appending an Array in Python
+print("Printing Appended Array:", one_array)
+print()
 
+one_array.pop()  # Removing last element from an Array
+print("Printing Array [After Pop()]:", one_array)
