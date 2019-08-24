46e956e3cc3dd39363bad02c48da7ed945858f11
Raghavendra Guttur <strikingraghu@gmail.com>
2019-08-24 21:53:20 +0530

    Python

---
+++ 02 Conditionals.py
@@ -0,0 +1,19 @@
+""" Conditionals """
+
+# samples
+a = 6
+if a < 5:
+    print('a < 5')
+else:
+    if a < 10:
+        print('5 <= a < 10')
+    else:
+        print('a >= 10')
+
+# ternary operator
+a, b = 10, 20
+get_output = a if a < b else b
+print(get_output)
+
+another_output = ({True: a, False: b}[a < b])
+print(another_output)
