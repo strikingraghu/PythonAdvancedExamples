7fb33193d148dc7b96f09f9bf7b1ffa98a393640
Raghavendra Guttur <strikingraghu@gmail.com>
2019-08-24 19:46:55 +0530

    Python

---
+++ 01 Multi-Line Statements.py
@@ -0,0 +1,23 @@
+""" Multi-Line Statements """
+
+a = [1, 2, 3]  # Normal way of creating a list
+
+b = [1,
+     2,
+     3]  # Another way to create a list
+
+c = [1,  # comment 1
+     2,  # comment 2
+     3]
+
+d = {'key1': 1,  # comment 1
+     'key2': 2  # comment 2
+     }
+
+# sample function
+
+
+def my_func(param_a,  # comment
+            param_b,  # comment
+            param_c):
+    print(param_a, param_b, param_c)
