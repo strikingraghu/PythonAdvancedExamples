1c1190be426eceae8bf6ba45ebf5c60e3901f214
Raghavendra Guttur <strikingraghu@gmail.com>
2019-08-26 16:56:58 +0530

    Python

---
+++ 03 Functions.py
@@ -0,0 +1,25 @@
+""" Functions """
+
+
+def func_1(a, b):
+    print(a, b)
+
+
+func_1(5, 10)
+
+
+def func_2():
+    return func_3()
+
+
+def func_3():
+    print('running func_3')
+
+
+# difference in output should be noticed
+func_2()
+print(func_2)
+
+# lambda
+sample_lambda = lambda x: x ** 5
+print(sample_lambda(2))
