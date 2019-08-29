404673ea9942095141395fc40a3350b2ccb9253e
Raghavendra Guttur <strikingraghu@gmail.com>
2019-08-29 22:10:35 +0530

    Python

--- 04 Loops.py
+++ 04 Loops.py
@@ -49,3 +49,32 @@ while var_one < 4:
     finally:
         print("{0}, {1} - Always executes!".format(var_one, var_two))
     print("{0}, {1} - Main loop!".format(var_one, var_two))
+
+
+# another sample
+list_2 = [(1, 3), (3, 5), (5, 7), (7, 9)]
+for each_tuple in list_2:
+    print("Each Element :", each_tuple)
+
+
+# another sample
+for sample1 in range(10):
+    if sample1 == 7:
+        break
+    print("Output Sample1 :", sample1)
+
+
+# another sample
+for sample2 in range(10):
+    if sample2 == 6:
+        continue
+    print("Output Sample2 :", sample2)
+
+
+# another sample
+for sample3 in range(1, 35):
+    print("Current Value :", sample3)
+    if sample3 % 7 == 0:
+        print("Multiple of 7 found in this code!")
+else:
+    print("Multiple of 7 not found in this code!")
